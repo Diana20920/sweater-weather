e0f8fb26d7e7d232055bf62e5754bf00e7aeaf3a
Diana Buffone <diana.pamelaet@gmail.com>
2021-06-14 12:05:00 -0400

    Connect to external api and create poro test

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,7 +4,10 @@ class Api::V1::BooksController < ApplicationController
     quantity = params[:quantity]
     conn = Faraday.get("http://openlibrary.org/search.json?q=#{destination}")
     response = JSON.parse(conn.body, symbolize_names: true)
-    response.map do |
+    total_found = response[:numFound]
+    search_results = response[:docs].map do |data|
+      BookSearch.new(total_found, destination, data)
+    end
     binding.pry
 
   end
