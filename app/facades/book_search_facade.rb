caccf9242b5c36f01ad3c5c23cb6bb3a137dd5cd
Diana Buffone <diana.pamelaet@gmail.com>
2021-06-14 16:23:59 -0400

    Create book search facade

---
+++ app/facades/book_search_facade.rb
@@ -0,0 +1,15 @@
+class BookSearchFacade
+  def self.search_by_destination(destination, quantity)
+    response = BooksService.search_books(destination)
+
+    total_found = response[:numFound]
+
+    forecast = ForecastFacade.current_weather(destination)
+
+    books = response[:docs].map do |data|
+      BookResult.new(data)
+    end.first(quantity)
+
+    BookSearch.new(total_found, destination, forecast, books)
+  end
+end
