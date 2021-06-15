fbcbaead739953771e02ecbcb788fae564cc86f6
Diana Buffone <diana.pamelaet@gmail.com>
2021-06-15 19:37:49 -0400

    Account for invalid params in forecast request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+  def invalid_params
+    render json: { error: 'Invalid parameters' }, status: :bad_request
+  end
+
   def invalid_credentials
     render json: { error: 'Invalid credentials' }, status: 400
   end
